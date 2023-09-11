1f564c45be6bf1fd664cdb7a308520d8b4495200
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-09-11 16:27:56 -0300

    Update funcs.c

--- lab3/funcs.c
+++ lab3/funcs.c
@@ -65,10 +65,24 @@ char get_next_char(FILE *arquivo, buffer_type *buffer) {
   } else if (current_char == '\n') { // Fim da linha
     get_next_buffer_block(arquivo, buffer);
     buffer->line++;
-    return get_next_char(arquivo, buffer);
+    return '\n';
   }
   
   return current_char;
+}
+
+/**
+ * verify functions
+**/
+
+int is_letter(char c) {
+    // Verifica se o caractere está no intervalo de 'a' a 'z' ou 'A' a 'Z'
+    return c != '\0' && c != EOF && c != '\n' &&(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+int is_number(char c) {
+    // Verifica se o caractere está no intervalo de '0' a '9'
+    return c != '\0' && c != EOF && c != '\n' && c >= '0' && c <= '9';
 }
 
 /**
@@ -105,10 +119,11 @@ int is_empty(char str[TOKEN_NAME_SIZE]) {
   return 1; // Todos os caracteres são nulos, a string está vazia
 }
 
-void insere_no_fim(token_type *token, char identificador[TOKEN_NAME_SIZE]) {
+void insere_no_fim(token_type *token, char identificador[], char type[]) {
   
   if (is_empty(token->token_name)) {
     strncpy(token->token_name, identificador, TOKEN_NAME_SIZE);
+    strncpy(token->type, type, TOKEN_TYPE_SIZE);
     token->prox = NULL;
     return;
   }
@@ -120,6 +135,7 @@ void insere_no_fim(token_type *token, char identificador[TOKEN_NAME_SIZE]) {
   }
 
   strncpy(novo_token->token_name, identificador, TOKEN_NAME_SIZE);
+  strncpy(novo_token->type, type, TOKEN_TYPE_SIZE);
   novo_token->prox = NULL;
 
   if (token == NULL) {
@@ -138,23 +154,14 @@ void insere_no_fim(token_type *token, char identificador[TOKEN_NAME_SIZE]) {
 void print_token_list(token_type *token)
 {
   token_type *atual = token;
-  printf("************ Token List start ************\n");
+  printf("**************** Token List start ****************\n");
+  printf("Token | Type\n");
   do {
     token_type *prox = atual->prox;
-    printf("%s\n", atual->token_name);
+    printf("%s | %s\n", atual->token_name, atual->type);
     atual = prox;
   } while (atual != NULL);
-  printf("************ Token List end ************\n");
-}
-
-int is_letter(char c) {
-    // Verifica se o caractere está no intervalo de 'a' a 'z' ou 'A' a 'Z'
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-}
-
-int is_number(char c) {
-    // Verifica se o caractere está no intervalo de '0' a '9'
-    return c >= '0' && c <= '9';
+  printf("**************** Token List end ****************\n");
 }
 
 /**
@@ -163,37 +170,41 @@ int is_number(char c) {
 
 void analyze_file(FILE *arquivo) {
   buffer_type *buffer = allocate_buffer();
-  token_type *token = allocate_token();
+  token_type *token_list = allocate_token();
   get_next_buffer_block(arquivo, buffer);
   char c;
   int aux=0;
-  char identificador[TOKEN_NAME_SIZE];
+  char token[TOKEN_NAME_SIZE];
   int dfa_state = 0;
   do {
     c = get_next_char(arquivo, buffer);
+    printf("%c dfa_state: %d aux: %d token: %s\n", c, dfa_state, aux, token);
     switch(dfa_state) {
       case 0:
+        memset(token, 0, TOKEN_NAME_SIZE);
+        aux=0;
         if (is_letter(c)) {
-          identificador[aux] = c;
+          token[aux] = c;
           dfa_state = 1;
           aux++;
         } else {
           dfa_state = 0;
-          // back_char_in_buffer(buffer);
+          aux=0;
+          printf("\'%c\' is not a identifier\n", c);
         }
         break;
       case 1:
         if (is_letter(c) || is_number(c)) {
-          identificador[aux] = c;
-          dfa_state = 1;
+          token[aux] = c;
           aux++;
         } else {
-          identificador[aux] = '\0';
-          insere_no_fim(token, identificador);
-          memset(identificador, 0, TOKEN_NAME_SIZE);
+          token[aux] = '\0';
+          printf("\'%s\' add on list\n", token);
+          insere_no_fim(token_list, token, "identificador");
+          memset(token, 0, TOKEN_NAME_SIZE);
           aux=0;
           dfa_state = 0;
-          // back_char_in_buffer(buffer);
+          back_char_in_buffer(buffer);
         }
         break;
       default:
@@ -201,8 +212,8 @@ void analyze_file(FILE *arquivo) {
     }
   } while (c != EOF);
 
-  print_token_list(token); // Imprime a lista de tokens
+  print_token_list(token_list); // Imprime a lista de tokens
   
   deallocate_buffer(buffer); // Desaloca a memoria do buffer
-  deallocate_token(token);  // Desaloca a memoria da tabela de tokens
+  deallocate_token(token_list);  // Desaloca a memoria da tabela de tokens
 }
